62f340541345ec9b6896f8da26a652810b627651
Nick Telenson <ntelenson@gmail.com>
2019-08-20 19:42:31 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -41,7 +41,7 @@ end
 def apply_clearance(cart)
   cart.keys.each do |item|
     if cart[item][:clearance]
-      (cart[item][:price] *= 0.8).round(2)
+      cart[item][:price] = (cart[item][:price]*.8)*round(2)
     end
   end
 end
